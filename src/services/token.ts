dc79e49af497f89703a7ec88f9eefb55b811ffd3
Davi Freitas <df828316@gmail.com>
2022-07-29 18:13:55 -0300

    refactor: :art: O token agora fica 'vivo' por apenas 7dias.

--- src/services/token.ts
+++ src/services/token.ts
@@ -1,9 +1,14 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions } from 'jsonwebtoken';
 import User from '../interfaces/user';
 
 const generateToken = (user: User): string => {
+  const payload = user;
   const secret = process.env.JWT_SECRET as string;
-  const token = jwt.sign(user, secret);
+  const options: SignOptions = {
+    algorithm: 'ES256',
+    expiresIn: '7d',
+  };
+  const token = jwt.sign(payload, secret, options);
   return token;
 };
 
